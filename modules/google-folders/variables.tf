c91330a12e40955c4bef37d8c7d43c1f1be25d9c
Anshu Priya <anshupriya609@gmail.com>
2024-10-29 02:32:36 +0530

    working version for folder and project creation

--- modules/google-folders/variables.tf
+++ modules/google-folders/variables.tf
@@ -1,33 +1,3 @@
-# variable "org_id" {
-#   type        = string
-#   description = "The Organization Id "
-# }
-
-# variable "folder_map" {
-#   type        = any
-#   description = "Folder structure as a map"
-# }
-
-
-# #new addition for projects
-# variable "billing_account_id" {
-#   type = string
-#   description = "Billing account ID for project creation"
-# }
-
-# variable "project_owner_email" {
-#   type = string
-#   description = "Email of the project owner for IAM"
-# }
-
-# variable "billing_account_user_email" {
-#   description = "The user email to be assigned billing permissions."
-#   type        = string
-# }
-
-
-
-#--------------------NEW APPROACH--------------------------------------
 variable "org_id" {
   description = "Organization ID"
   type        = string
